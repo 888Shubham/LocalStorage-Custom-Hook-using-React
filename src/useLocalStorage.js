fcc49a7c8a5d53c3ab863fb5a626128beeb2235a
888Shubham <shubham.kb888@gmail.com>
2024-03-12 14:10:53 +0530

    first commit

---
+++ src/useLocalStorage.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+const useLocalStorage = (key, defaultValue) => {
+  // Add the state and effect here
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(key);
+
+    if (!jsonValue) return defaultValue;
+
+    return JSON.parse(jsonValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
